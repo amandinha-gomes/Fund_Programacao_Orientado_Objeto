af86281ada9a29da96d64b6986c156c6f5d604d5
Amanda <134006882+amandinha-gomes@users.noreply.github.com>
2023-07-27 11:44:46 -0300

    diarista

--- projetoDiarista/src/br/com/classes/Pessoa.java
+++ projetoDiarista/src/br/com/classes/Pessoa.java
@@ -1,9 +1,42 @@
 package br.com.classes;
 
-public class Pessoa {
-	
-	public String nome;
-	public String telefone;
-	public String endereço;
+import br.com.classes.interfaces.Isaldo;
 
+abstract class Pessoa implements Isaldo{
+	
+	protected String nome;
+	protected String telefone;
+	protected String endereço;
+	private double saldo;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getEndereço() {
+		return endereço;
+	}
+	
+	public void setEndereço(String endereço) {
+		this.endereço = endereço;
+	}
 }
--- projetoDiarista/src/br/com/projeto/App.java
+++ projetoDiarista/src/br/com/projeto/App.java
@@ -1,5 +1,6 @@
 package br.com.projeto;
 
+import br.com.classes.Atendimento;
 import br.com.classes.Cliente;
 import br.com.classes.Diarista;
 
@@ -9,30 +10,50 @@ public class App {
 		
 		Diarista diarista = new Diarista();
 		
-		diarista.nome  = "Joana";
-		diarista.telefone = "(11) 9002-8922";
-		diarista.endereço = "Av Cajamar, N 111";
-		diarista.chavepix = "123";
+		diarista.setNome("Joana");
+		diarista.setTelefone("(11) 9002-8922");
+		diarista.setEndereço("Av Cajamar, N 111");
+		diarista.setChavepix("123");
 		
-		System.out.println("Nome Diarista: " + diarista.nome);
-		System.out.println("Telefone: " + diarista.telefone);
-		System.out.println("Endereço: " + diarista.endereço);
-		System.out.println("Chave Pix Diarista: " + diarista.chavepix);
+		System.out.println("Nome Diarista: " + diarista.getNome());
+		System.out.println("Telefone: " + diarista.getTelefone());
+		System.out.println("Endereço: " + diarista.getEndereço());
+		System.out.println("Chave Pix Diarista: " + diarista.getChavepix());
 		diarista.atender("Daniel");
+		
+		//valor na conta diarista//
+		System.out.println("Saldo Cliente: " + diarista.getSaldo());
+		diarista.depositar(30);
+		System.out.println("Novo saldo: " + diarista.getSaldo());
+		diarista.sacar(10);
+		System.out.println("Novo saldo: " + diarista.getSaldo());
 		
 		System.out.println("=============================================");
 		
 		Cliente cliente = new Cliente();
 		
-		cliente.nome = "Daniel";
-		cliente.telefone = "(11) 99875-7857";
-		cliente.endereço = "Portal dos Ipes, N 35";
-		cliente.saldo = 100.00;
+		cliente.setNome("Daniel");
+		cliente.setTelefone("(11) 99875-7857");
+		cliente.setEndereço("Portal dos Ipes, N 35");
 		
-		System.out.println("Nome Cliente: " + cliente.nome);
-		System.out.println("Telefone: " + cliente.telefone);
-		System.out.println("Endereço: " + cliente.endereço);
-		System.out.println("Saldo Cliente: " + cliente.saldo);
+		System.out.println("Nome Cliente: " + cliente.getNome());
+		System.out.println("Telefone: " + cliente.getTelefone());
+		System.out.println("Endereço: " + cliente.getEndereço());
+		
+		//valor na conta cliente//
+		System.out.println("Saldo Cliente: " + cliente.getSaldo());
+		cliente.depositar(30);
+		System.out.println("Novo saldo: " + cliente.getSaldo());
+		cliente.sacar(10);
+		System.out.println("Novo saldo: " + cliente.getSaldo());
+		
+		
+		System.out.println("=============================================");
+		
+		Atendimento atendimento = new Atendimento();
+		
+		atendimento.setHoras(0);
+		System.out.println(atendimento.getHoras());
 		
 	}
 }
